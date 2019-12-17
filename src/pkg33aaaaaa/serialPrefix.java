d79b65a37dab3395f7393399de805067f9f8c929
asmaafarrag <48455830+asmaafarrag@users.noreply.github.com>
2019-12-17 21:31:08 +0200

    Update serialPrefix.java

--- src/pkg33aaaaaa/serialPrefix.java
+++ src/pkg33aaaaaa/serialPrefix.java
@@ -1,47 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pkg33aaaaaa;
 
+package pkg33aaaaaa;
+import java.util.Arrays;
 /**
  *
  * @author asmaa
  */
-public class serialPrefix extends Thread
+public class serialPrefix 
 {
-     
-    
-	// Fills prefix sum array 
-    static void serialPrefixSum(int arr[], int n,int prefixSum[]) 
+   
+    static void serialPrefixSum(int input[], int n,int output[]) 
     { 
-		prefixSum[0] = arr[0]; 
+		output[0] = input[0]; 
 		
-		// Adding present element 
-		// with previous element 
+		// Adding present element with previous element 
 		for( int i = 1; i < n; ++i ) 
-			prefixSum[i] = prefixSum[i-1] + arr[i]; 
+			output[i] = output[i-1] + input[i]; 
     } 
         
      
 	
-	// Driver code 
+	
 	public static void main(String[] args) 
 	{ 
-		int arr[] = { 10, 4, 16, 20 }; 
-		int n = arr.length; 
-		int prefixSum[] = new int[n]; 
-		
-		serialPrefixSum(arr, n, prefixSum); 
+		int input[] = { 9,8,3,2,7,1,6,4 }; 
+		int n = input.length; 
+		int output[] = new int[n]; 
+		System.out.println(Arrays.toString(input));
+                
+                
+		serialPrefixSum(input, n, output); 
                 
                 //parallelprefixsum(arr, n, prefixSum);
 		long start = System.currentTimeMillis();
                 
-		for (int i = 0; i < n; i++) {
-			System.out.print( prefixSum[i] + " "); 
-		System.out.println(""); }
-                
+		System.out.println(Arrays.toString(output));
+                        
+  
                 System.out.println("Execution Time: " + (System.currentTimeMillis() - start)+ " seconds.");
 	} 
 
