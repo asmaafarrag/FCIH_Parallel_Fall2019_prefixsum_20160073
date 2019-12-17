6fd4871a8fb69fe64ffbb3454e29afad0e569109
asmaafarrag <48455830+asmaafarrag@users.noreply.github.com>
2019-12-17 21:30:02 +0200

    Update mainnew.java

--- src/pkg33aaaaaa/mainnew.java
+++ src/pkg33aaaaaa/mainnew.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pkg33aaaaaa;
 
+package pkg33aaaaaa;
+import java.util.Scanner;
 import java.util.Date;
 import java.util.Arrays;
 import java.util.concurrent.CountDownLatch;
@@ -21,37 +17,30 @@ public class mainnew  {
     
     
      public static void main(String[] args) throws InterruptedException {
-        // TODO code application logic here
-//        int n = 16;
-        int n = 8; // arraysize
-
-        int InArr[] = new int[]{4,9,5,1,0,5,1,6,6,4,6,5,1,6,9,3};
-        int input[] = new int[]{6,4,16,10,16,14,2,8};
+        
+        int n = 8;
+        int input[] = new int[]{9,8,3,2,7,1,6,4};
+        //Scanner in = new Scanner(System.in);
+        //int  inp = in.nextInt();
+        //int input[]=new int [inp];
         int output[] = new int[n];
         
         
         int numofthreads = Runtime.getRuntime().availableProcessors();
-         executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(numofthreads);
-        //double m;
-       // m = Math.ceil( (double)n/TNum );
-        int size = n/numofthreads ;
-        int Prefixsum[] = new int[numofthreads];
-        
-        int startIndex=0, endIndex=size;
-        
         CountDownLatch latch = new CountDownLatch(numofthreads);
         CountDownLatch latch2 = new CountDownLatch(numofthreads);
-         
+        
+         executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(numofthreads);
+        int size = n/numofthreads ;
+        int Prefixsum[] = new int[numofthreads];
+        int startIndex=0, endIndex=size;
+        
+        
+
         double currentTime = 0d;
         
          Date start, end;
          start = new Date();
-         
-    //1st Parallel Version
-    //System.out.println(numofthreads);
-    
-        
-        
     for(int i = 0 ; i < numofthreads ; i++)
         {
             running Task = new running(startIndex ,endIndex ,n,i,1,0,input,output,Prefixsum,latch);
@@ -67,17 +56,11 @@ public class mainnew  {
 
         }
        latch.await();
-        //System.out.println("HI122"); 
-        
-       //serial prefixsum lel prefixarr
         for(int i = 1 ; i<numofthreads; i++)
         {
             Prefixsum[i]+=Prefixsum[i-1];
         }
 
-        //System.out.println("HI3");
-        
-        //2nd paralell prefixsum
     
         startIndex=size;
         endIndex=startIndex+size;
@@ -99,9 +82,7 @@ public class mainnew  {
        currentTime = end.getTime() - start.getTime();
       System.out.println(Arrays.toString(input));
       System.out.println(Arrays.toString(output));
-      //System.out.println(Arrays.toString(PrefixArr));
-      System.out.println("parallel Time: " + (currentTime)
-                + " seconds.");
+      System.out.println("parallel Time: " + (currentTime) + " seconds.");
      
         currentTime = 0d;
         
@@ -112,8 +93,7 @@ public class mainnew  {
         
          currentTime = end.getTime() - start.getTime();
         
-     System.out.println("serial Time: " + (currentTime)
-                + " seconds."); 
+     System.out.println("serial Time: " + (currentTime)+ " seconds."); 
      
      executor.shutdown();
 }}
